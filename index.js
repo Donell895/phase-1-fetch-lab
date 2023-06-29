bcc17feecf1c1826ed0c22bc76e4dbad3d1d927a
donell <donell.wambua@student.moringaschool.com>
2023-06-29 08:55:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch ('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(thrones =>(
+    renderBooks(thrones))
+  )
 }
+function renderBooks(books) {
+  // Access the bookList element in the DOM
+  const bookListElement = document.getElementById('bookList');
+
+  // Clear any existing content inside the element
+  bookListElement.innerHTML = '';
+
+  // Iterate through each book in the JSON object
+  books.forEach(book => {
+    // Create a new list item element for each book
+    const listItem = document.createElement('li');
+    listItem.textContent = book.title; // Assuming the book object has a "title" property
+
+    // Append the list item to the bookList element
+    bookListElement.appendChild(listItem);
+  });
+}
+const booksData = [
+  { "title": "Book 1" },
+  { "title": "Book 2" },
+  { "title": "Book 3" }
+];
+
+renderBooks(booksData);
+
+
+
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
